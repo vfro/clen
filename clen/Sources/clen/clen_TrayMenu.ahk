2372d6f7f866065118c3f49c0a50c1135ddaa8a6
Volodymyr Frolov <frolov.volodymyr@gmail.com>
2010-10-20 23:09:36 +0300

    Issue #17 (starting clen v2.0 UNICODE)

--- clen/Sources/clen/clen_TrayMenu.ahk
+++ clen/Sources/clen/clen_TrayMenu.ahk
@@ -1,4 +1,4 @@
-; Copyright 2008-2009 Volodymyr Frolov
+; Copyright 2008-2010 Volodymyr Frolov
 ; Licensed under the Apache License, Version 2.0 (the "License");
 ; you may not use this file except in compliance with the License.
 ; You may obtain a copy of the License at
@@ -11,7 +11,7 @@
 
 clen_InitializeTrayMenu()
 {
-  Menu, Tray, Tip, Clipboard Enhanced v1.1 © 2008-2009 Volodymyr Frolov
+  Menu, Tray, Tip, Clipboard Enhanced v2.0 © 2008-2010 Volodymyr Frolov
 
   Menu, tray, NoStandard
 
--- clen/Sources/clen_make_executable.cmd
+++ clen/Sources/clen_make_executable.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-rem Copyright 2008-2009 Volodymyr Frolov
+rem Copyright 2008-2010 Volodymyr Frolov
 rem Licensed under the Apache License, Version 2.0 (the "License");
 rem you may not use this file except in compliance with the License.
 rem You may obtain a copy of the License at
@@ -11,17 +11,17 @@ rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 rem See the License for the specific language governing permissions and
 rem limitations under the License.
 
-taskkill /IM clipboard_enhanced-v1.1.exe > nul 2>&1
+taskkill /IM clipboard_enhanced-v2.0.exe > nul 2>&1
 
-type clen\clen_Initialize.ahk > clen.ahk
-type clen\clen_Numpad.ahk >> clen.ahk
-type clen\clen_CopyPaste.ahk >> clen.ahk
-type clen\clen_DynamicClipboards.ahk >> clen.ahk
-type clen\clen_StaticClipboards.ahk >> clen.ahk
-type clen\clen_RegularClipboard.ahk >> clen.ahk
-type clen\clen_PasswordClipboard.ahk >> clen.ahk
-type clen\clen_SaveLoad.ahk >> clen.ahk
-type clen\clen_TrayMenu.ahk >> clen.ahk
+type clen\clen_Initialize.ahk > clen-all.ahk
+type clen\clen_Numpad.ahk >> clen-all.ahk
+type clen\clen_CopyPaste.ahk >> clen-all.ahk
+type clen\clen_DynamicClipboards.ahk >> clen-all.ahk
+type clen\clen_StaticClipboards.ahk >> clen-all.ahk
+type clen\clen_RegularClipboard.ahk >> clen-all.ahk
+type clen\clen_PasswordClipboard.ahk >> clen-all.ahk
+type clen\clen_SaveLoad.ahk >> clen-all.ahk
+type clen\clen_TrayMenu.ahk >> clen-all.ahk
 
-Ahk2Exe.exe /in clen.ahk /icon ../Resources/clen.ico /out clipboard_enhanced-v1.1.exe /NoDecompile
-start clipboard_enhanced-v1.1.exe
+Ahk2Exe.exe /in clen.ahk /icon ../Resources/clen.ico /out clipboard_enhanced-v2.0.exe /NoDecompile
+start clipboard_enhanced-v2.0.exe
